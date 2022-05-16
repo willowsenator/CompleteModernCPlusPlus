cb985f580baa5ae40059833a75002e74fabc8990
willowsenator <willowsenator@gmail.com>
2022-05-16 23:56:16 +0200

    Add Checking account

---
+++ Complete_Modern_Cplusplus/Account/Checking.cpp
@@ -0,0 +1,24 @@
+﻿#include "Checking.h"
+
+#include <iostream>
+#include <utility>
+
+
+Checking::Checking(std::string _name, const float _balance): Account(std::move(_name), _balance)
+{
+}
+
+Checking::~Checking()
+= default;
+
+void Checking::withdraw(float amount)
+{
+    if(balance_ - amount >= 50)
+    {
+        balance_ -= amount;
+    }
+    else
+    {
+        std::cout << "Insufficient Balance!! You can leave account with less than 50!!" << std::endl;
+    }
+}
---
+++ Complete_Modern_Cplusplus/Account/Checking.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Account.h"
+
+class Checking:public Account
+{
+public:
+    Checking(std::string _name, float balance);
+    ~Checking();
+    void withdraw(float amount);
+};
